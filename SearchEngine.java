629f113e2b51fc8aa71119c13c1364c9f15a2801
gdietz8 <83431340+gdietz8@users.noreply.github.com>
2023-04-07 14:27:13 -0700

    hi

---
+++ SearchEngine.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    List<String> stringList = new ArrayList<String>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Genevieve's List: %s", stringList);
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    stringList.add(parameters[1]);
+                    return String.format("Added %s! It's now %s", parameters[1], stringList);
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+public class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
